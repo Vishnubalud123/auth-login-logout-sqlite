a36832b9deebac1fb8ded76dfd66e330ccf882f6
Vishnubalu.D <143685575+Vishnubalud123@users.noreply.github.com>
2025-09-04 21:11:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || https://auth-login-logout-sqlite-5uis.vercel.app/;
 
 app.use(cors({
   origin: 'https://auth-login-logout-sqlite-5uis.vercel.app/',
